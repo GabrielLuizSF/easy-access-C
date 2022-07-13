f8dd1f97a1630b207596e197d591850c5eda9f73
Gabriel Luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-07-13 06:39:13 -0300

    Update styleLib

--- library/styleLib.h
+++ library/styleLib.h
@@ -4,8 +4,8 @@
 
 //cores em ANSI utilizadas
 #define ANSI_COLOR_RESET   "\x1b[0m"
-#define ANSI_COLOR_RED     	"\x1b[31m"  
-#define ANSI_COLOR_GREEN	"\e[0;32m"
+#define ANSI_COLOR_RED     "\x1b[31m"  
+#define ANSI_COLOR_GREEN   "\e[0;32m"
 #define ANSI_COLOR_CYAN     "\e[0;36m"
 #define ANSI_COLOR_PURPLE   "\e[1;35m"
 #define ANSI_COLOR_YELLOW   "\e[0;33m"
@@ -13,30 +13,27 @@
 
 namespace stylelib{
 	void styleRed(){
-		std:: cout<<ANSI_COLOR_RED<< std:: endl;
+	  std:: cout<<ANSI_COLOR_RED<< std:: endl;
 
 	}
-		void styleBlue(){
-		std:: cout<<ANSI_COLOR_BLUE<< std:: endl;
+	void styleBlue(){
+           std:: cout<<ANSI_COLOR_BLUE<< std:: endl;
 
-	}
-		void styleGreen(){
-		std:: cout<<ANSI_COLOR_GREEN<< std:: endl;
-
-	}
-		void styleYellow(){
-		std:: cout<<ANSI_COLOR_YELLOW<< std:: endl;
-
-	}
-		void stylePurple(){
-		std:: cout<<ANSI_COLOR_PURPLE<< std:: endl;
-
-	}
+         }
+	 void styleGreen(){
+	     std:: cout<<ANSI_COLOR_GREEN<< std:: endl;
+          }
+	void styleYellow(){
+	     std:: cout<<ANSI_COLOR_YELLOW<< std:: endl;
+           }
+	void stylePurple(){
+	     std:: cout<<ANSI_COLOR_PURPLE<< std:: endl;
+           }
 	void styleCyan(){
-		std:: cout <<ANSI_COLOR_CYAN<< std:: endl;
-	}
+	     std:: cout <<ANSI_COLOR_CYAN<< std:: endl;
+         }
 	void resetStyle(){
-		std::cout<<ANSI_COLOR_RESET<<std::endl;
+	     std::cout<<ANSI_COLOR_RESET<<std::endl;
 	}
 }
 #endif
